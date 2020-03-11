7769fc177fd98aa5ab9e1515a2d5b59d70c685b2
Zhangzhe <zhangzhe0177@gmail.com>
2020-03-11 17:56:21 +0800

    0.4-dev1

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 // ==UserScript==
 // @name         csscloud flash 播放器替换
 // @namespace    https://home.asec01.net/
-// @version      0.3-2
-// @description  将 csscloud 的 flash 播放器换为 flvjs
+// @version      0.4-dev1
+// @description  将 csscloud 的 flash 播放器换为 DPlayer
 // @author       Zhe Zhang
 // @match        http://view.csslcloud.net/api/view/*
 // @match        https://view.csslcloud.net/api/view/*
-// @grant        none
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.0/jquery.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/flv.js/1.5.0/flv.min.js
+// @resource     dPlayerCSS https://cdnjs.cloudflare.com/ajax/libs/dplayer/1.25.0/DPlayer.min.css
+// @require      https://cdnjs.cloudflare.com/ajax/libs/dplayer/1.25.0/DPlayer.min.js
 // ==/UserScript==
-
 (function () {
+
     function getQueryVariable(variable) {
         var query = window.location.search.substring(1);
         var vars = query.split("&");
@@ -23,18 +27,46 @@
         return (false);
     }
 
-    function playLink(u, t) {
-        zzlog("播放链接:\n" + u);
-        if (flvjs.isSupported()) {
-            var videoElement = document.getElementById('videoElement');
-            var flvPlayer = flvjs.createPlayer({
-                type: t,
-                url: u
-            });
-            flvPlayer.attachMediaElement(videoElement);
-            flvPlayer.load();
-            flvPlayer.play();
-        }
+    var dp;
+
+    function playLive(u) {
+        zzlog("playLive播放链接:\n" + u);
+        dp = new DPlayer({
+            container: document.getElementById('videoElement'),
+            autoplay: true,
+            live: true,
+            danmaku: true,
+            video: {
+                url: u,
+            },
+        });
+    }
+
+    function playLink(u) {
+        zzlog("playLink播放链接:\n" + u);
+        dp = new DPlayer({
+            container: document.getElementById('videoElement'),
+            autoplay: true,
+            live: false,
+            danmaku: true,
+            video: {
+                url: u,
+            },
+        });
+    }
+
+    function addDanmaku(t){
+        const danmaku = {
+            text: t,
+            color: '#ffffff',
+            type: 'right',
+        };
+        dp.danmaku.draw(danmaku);
+    }
+
+    const hijack = (obj, method, fun) => {
+        let orig = obj[method]
+        obj[method] = fun(orig)
     }
 
     function zzlog(t) {
@@ -60,19 +92,24 @@
     zzlog("isHttps: " + isHttps);
 
     $(document).ready(function () {
-        zzlog("Dom加载完成，替换播放器");
+        zzlog("Dom加载完成");
         var livePlayer = $('#doc-main');
         if (livePlayer.length == 1) {
-            $(livePlayer).html('<video id="videoElement" height="100%" width="100%" autoplay controls></video>');
+            // $(livePlayer).html('<video id="videoElement" height="100%" width="100%" autoplay controls></video>');
+            $(livePlayer).html('<div id="videoElement"></div>');
         }
-        zzlog("替换完成");
+        var dPlayerCSS = GM_getResourceText("dPlayerCSS");
+        GM_addStyle(dPlayerCSS);
+        GM_addStyle(".videoElement { width: 100%; height: 100%; }");
+        GM_addStyle(".dplayer { width: 100%; height: 100%; }");
+        GM_addStyle(".video-middle { background-color: black; }");
 
         if (recordId == false) {
             if (roomId == false) {
                 zzlog("参数错误 - 未获取到roomid和recordId");
             } else {
                 zzlog("直播模式");
-                playLink('//stream-ali1.csslcloud.net/src/' + roomId + '.flv', "flv");
+                playLive('//stream-ali1.csslcloud.net/src/' + roomId + '.flv');
             }
         } else {
             zzlog("回放模式");
@@ -93,9 +130,26 @@
                     } else {
                         link = linkObj["playurl"];
                     }
-                    playLink(link, "mp4");
+                    playLink(link);
                 }
             });
         }
+
+        var targetNode = document.getElementById('chat-list');
+        var config = {attributes: true, childList: true, subtree: true};
+        const mutationCallback = (mutationsList) => {
+            for (let mutation of mutationsList) {
+                if (mutation.type == "childList") {
+                    // console.log("A child node has been added or removed.");
+                    var nodeElem = mutation.addedNodes[1];
+                    console.log(nodeElem);
+                    var innerText = nodeElem.getElementsByClassName("peo-chat")[0].innerText;
+                    console.log(innerText);
+                    addDanmaku(innerText);
+                }
+            }
+        };
+        var observer = new MutationObserver(mutationCallback);
+        observer.observe(targetNode, config);
     })
 })();
